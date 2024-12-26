0ce189ef724a72659029390ffea823794a851bcb
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2024-12-26 22:29:15 +0500

    updating the signup page

--- frontend/quickbite_web/src/components/SignUp.jsx
+++ frontend/quickbite_web/src/components/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constant";
+import { assets } from "../assets/images/assets";
 const SignUp = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -39,87 +40,94 @@ const SignUp = () => {
 
   return (
     <>
-      <section className="signup bg-Image">
+      <section className="signup">
         <div className="container">
-          <div className="w-[390px] m-auto px-[39px] py-[45px] border border-[#808080] rounded-2xl bg-opacity-90 shadow-2xl">
-            <h1 className="text-[24px] text-center text-gray-800">
-              Create an account
-            </h1>
-            <p className="text-[14px] font-[700] text-center mt-[7px] text-gray-500">
-              Get Your Favorite Meals Delivered Fast!
-            </p>
-            <form className="mt-[36px] block" onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-                className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px]  focus:outline-none focus:ring-2 focus:ring-blue-500 "
-                placeholder="First Name"
-                required
-              />
-              <input
-                type="text"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px]  focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Last Name"
-                required
-              />
-              <input
-                type="email"
-                value={emailId}
-                onChange={(e) => setEmail(e.target.value)}
-                className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px]  focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Email Id"
-                required
-              />
-              <input
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px]  focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Password"
-                required
-              />
-              <select
-                id="options"
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px] text-sm text-[#808080]  focus:outline-none focus:ring-2 focus:ring-blue-500"
-              >
-                <option value="" disabled>
-                  Select an option
-                </option>
-                <option className="text-[#808080]" value="user">
-                  User
-                </option>
-                <option className="text-[#808080]" value="admin">
-                  Admin
-                </option>
-              </select>
-              <div className="flex items-center gap-x-[5px]">
-                <input
-                  type="checkbox"
-                  className="relative left-0 top-1"
-                  checked={showPassword}
-                  onChange={handleShowPassword}
-                />
-                <p className="text-sm text-[#808080] mt-[8px]">show password</p>
-              </div>
-
-              <button
-                type="submit"
-                className="w-full py-[12px] px-[123px] bg-[#0E64D2] text-[#FFFFFF] mt-[29px] mb-[32px] rounded-[5px] text-[16px] font-[500]"
-              >
-                Sign Up
-              </button>
-              <p className="text-center">
-                Already have an account?{" "}
-                <Link className="text-[#2F89FC]" to="/login">
-                  Login
-                </Link>
+          <div className="flex justify-center items-center gap-x-[50px] border-2 border-[#808080] h-[700px] rounded-2xl bg-opacity-90 shadow-2xl  px-[39px] py-[45px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  w-[1000px]">
+            <div className="">
+              <h1 className="text-[24px] text-center text-gray-800">
+                Create an account
+              </h1>
+              <p className="text-[14px] font-[700] text-center mt-[7px] text-gray-500">
+                Get Your Favorite Meals Delivered Fast!
               </p>
-            </form>
+              <form className="mt-[36px] block" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px]  focus:outline-none focus:ring-2 focus:ring-blue-500 "
+                  placeholder="First Name"
+                  required
+                />
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px]  focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Last Name"
+                  required
+                />
+                <input
+                  type="email"
+                  value={emailId}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px]  focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Email Id"
+                  required
+                />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px]  focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Password"
+                  required
+                />
+                <select
+                  id="options"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                  className="border border-[#808080] block w-full py-[12px] px-[11px] text-[16px] outline-none rounded-[10px] mt-[28px] text-sm text-[#808080]  focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                  <option value="" disabled>
+                    Select an option
+                  </option>
+                  <option className="text-[#808080]" value="user">
+                    User
+                  </option>
+                  <option className="text-[#808080]" value="admin">
+                    Admin
+                  </option>
+                </select>
+                <div className="flex items-center gap-x-[5px]">
+                  <input
+                    type="checkbox"
+                    className="relative left-0 top-1"
+                    checked={showPassword}
+                    onChange={handleShowPassword}
+                  />
+                  <p className="text-sm text-[#808080] mt-[8px]">
+                    show password
+                  </p>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full py-[12px] px-[123px] bg-[#0E64D2] text-[#FFFFFF] mt-[29px] mb-[32px] rounded-[5px] text-[16px] font-[500]"
+                >
+                  Sign Up
+                </button>
+                <p className="text-center">
+                  Already have an account?{" "}
+                  <Link className="text-[#2F89FC]" to="/login">
+                    Login
+                  </Link>
+                </p>
+              </form>
+            </div>
+            <div>
+              <img src={assets.food_bg} alt=" bg image" />
+            </div>
           </div>
         </div>
       </section>
